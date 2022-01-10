13894954e3739e42eec53d2db2876cc8bc95f259
ovichowdhury <ovi.chowdhury@hotmail.com>
2022-01-10 11:01:48 +0600

    complex joi validation demo added

---
+++ pages/realjoi/complex-validation.jsx
@@ -0,0 +1,59 @@
+import Joi from 'joi';
+import React from 'react';
+
+
+function ComplexValidation() {
+
+
+  const post = Joi.object().keys({
+    title: Joi.string().required(),
+    desc: Joi.string().required()
+  });
+
+  const user = Joi.object({
+    name: Joi.string().required(),
+    posts: Joi.array().items(post)
+  });
+
+  const onCheckClick = (e) => {
+    const userData = {
+      name: 'nahid',
+      posts: [
+        { title: "null", desc: 'my post' },
+        { title: null, desc: 'my post' },
+        { title: 'my post', desc: 'my post' }
+      ]
+    };
+
+    const { error, value } = user.validate(userData, {
+      abortEarly: false
+    });
+    console.log(error.details);
+  }
+
+  return (
+    <>
+      <div style={styles.flexDiv}>
+        <button style={styles.myButton} onClick={onCheckClick}>Check</button>
+      </div>
+    </>
+  );
+}
+
+const styles = {
+  flexDiv: {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '50px',
+    padding: '50px',
+    border: '1px solid gray'
+  },
+  myButton: {
+    backgroundColor: 'black',
+    color: 'white',
+    padding: '10px',
+    cursor: 'pointer'
+  }
+}
+
+export default ComplexValidation;
