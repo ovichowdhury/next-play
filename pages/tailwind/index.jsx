8e38a4d26dfae1f0569c8e27da03daeada028bde
ovichowdhury <ovi.chowdhury@hotmail.com>
2022-01-24 13:37:05 +0600

    tailwind state changes example added

--- pages/tailwind/index.jsx
+++ pages/tailwind/index.jsx
@@ -3,15 +3,15 @@
 function Index() {
   return (
     <>
-      <div className='h-screen flex items-center justify-center bg-gray-100'>
-        <div className='max-w-sm mx-auto p-8 sm:p-4 bg-white rounded-xl shadow-lg space-y-2 sm:flex sm:space-y-0 sm:space-x-4'>
-          <img className='h-28 sm:h-24 mx-auto rounded-full ring-4 ring-yellow-200 sm:flex-shrink-0' src="/img/pizza.png" alt="image"/>
-          <div className='text-center space-y-2 '>
+      <div className='flex items-center justify-center h-screen bg-gray-100 dark:bg-gray-800'>
+        <div className='max-w-sm p-8 mx-auto space-y-2 bg-white shadow-lg sm:p-6 rounded-xl sm:flex sm:space-y-0 sm:space-x-4 '>
+          <img className='mx-auto duration-300 transform rounded-full h-28 sm:h-24 ring-4 ring-yellow-200 sm:flex-shrink-0 hover:scale-105' src="/img/pizza.png" alt="image" />
+          <div className='space-y-2 text-center '>
             <div className='space-y-1 sm:space-y-0'>
-              <p className='text-lg text-black font-semibold'>Halal Heaven's</p>
-              <p className='text-gray-500 font-medium'>Youtube channel</p>
+              <p className='text-lg font-semibold text-black'>Halal Heaven's</p>
+              <p className='font-medium text-gray-500'>Youtube channel</p>
             </div>
-            <button className='py-1 px-4 border border-purple-200 rounded-full text-sm font-semibold text-purple-600'>Visit now</button>
+            <button className='px-4 py-1 text-sm font-semibold text-purple-600 border border-purple-200 rounded-full hover:border-transparent hover:text-white hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2'>Visit now</button>
           </div>
         </div>
       </div>
