b4722071452c00c146ff2534c387d9f7e2b62db5
unknown <ovi.chowdhury@hotmail.com>
2022-04-16 17:15:35 +0600

    s3 presigned url put and get demo added

---
+++ pages/api/s3/[file].js
@@ -0,0 +1,26 @@
+import nc from "next-connect";
+import s3 from "../../../lib/s3";
+
+const handler = nc({
+  onError: (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).end("Error");
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).end("Not Found");
+  },
+});
+
+handler.get(async (req, res) => {
+  const getUrl = s3.getSignedUrl("getObject", {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: req.query.file, //filename
+    Expires: 300, //time to expire in seconds
+  });
+  res.status(200).json({
+    message: "success",
+    getUrl: getUrl,
+  });
+});
+
+export default handler;
