825692ef983b9e743e349951a4345d42e8ef8707
unknown <ovi.chowdhury@hotmail.com>
2022-05-15 01:03:43 +0600

    Sidebar design added

--- components/common/sidebar/Sidebar.jsx
+++ components/common/sidebar/Sidebar.jsx
@@ -1,5 +1,61 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+import React, { useState } from "react";
 
-export default function Sidebar() {
-  return <div>Sidebar</div>;
+export default function Sidebar({ menus }) {
+  const [open, setOpen] = useState(true);
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+  return (
+    <div className="flex">
+      <div
+        className={`relative h-screen bg-gray-50 ${
+          open ? "w-64" : "w-20"
+        } duration-300 p-5 pt-8`}
+      >
+        {/**Control button */}
+        <img
+          src="/sidebar/control.png"
+          alt="Control"
+          className={`absolute cursor-pointer w-7 border-2 border-gray-200 -right-3 top-9 rounded-full ${
+            !open && "rotate-180"
+          }`}
+          onClick={toggleOpen}
+        />
+
+        {/**Brand Area */}
+        <div className="flex items-center gap-x-4">
+          <img
+            src="/sidebar/logo.png"
+            alt="Logo"
+            className={`cursor-pointer duration-500`}
+          />
+          <h1
+            className={`text-black origin-left font-medium text-xl duration-300 uppercase ${
+              !open && "scale-0"
+            }`}
+          >
+            Property
+          </h1>
+        </div>
+        {/** Menus */}
+        <ul className="pt-6">
+          {menus.map((menu, index) => (
+            <li
+              key={index}
+              className={`flex  rounded-md p-2 cursor-pointer hover:bg-slate-200 text-gray-600 text-md font-semibold items-center gap-x-4 
+              ${menu.gap ? "mt-9" : "mt-2"} `}
+            >
+              <img src={`/sidebar/${menu.src}.png`} alt="icon" />
+              <span className={`${!open && "hidden"} origin-left duration-200`}>
+                {menu.title}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/** Right Panel */}
+      <div className="p-7 font-semibold text-2xl flex-1 h-screen">Homepage</div>
+    </div>
+  );
 }
--- pages/sidebar/index.js
+++ pages/sidebar/index.js
@@ -1,10 +1,22 @@
+import Navbar from "@components/common/navbar/Navbar";
 import Sidebar from "@components/common/sidebar/Sidebar";
 import React from "react";
 
 export default function Index() {
+  const menus = [
+    { title: "Dashboard", src: "Chart_fill" },
+    { title: "Inbox", src: "Chat" },
+    { title: "Accounts", src: "User", gap: true },
+    { title: "Schedule ", src: "Calendar" },
+    { title: "Search", src: "Search" },
+    { title: "Analytics", src: "Chart" },
+    { title: "Files ", src: "Folder", gap: true },
+    { title: "Setting", src: "Setting" },
+  ];
   return (
     <div>
-      <Sidebar />
+      <Navbar />
+      <Sidebar menus={menus} />
     </div>
   );
 }
