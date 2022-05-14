20fc4359b9ae8dd421f85074dddadf146027114c
unknown <ovi.chowdhury@hotmail.com>
2022-05-14 23:44:45 +0600

    feat: sidebar design in progress

---
+++ pages/sidebar/index.js
@@ -0,0 +1,10 @@
+import Sidebar from "@components/common/sidebar/Sidebar";
+import React from "react";
+
+export default function Index() {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+}
