e7c0799507e9d826596c41086c36c40dadeed63b
ovichowdhury <ovi.chowdhury@hotmail.com>
2023-01-25 10:17:08 +0600

    feat: camera component api call added

--- pages/camera/custom.jsx
+++ pages/camera/custom.jsx
@@ -1,10 +1,29 @@
-import Camera from '@components/camera/Camera'
-import React from 'react'
+import Camera from "@components/camera/Camera";
+import React from "react";
 
 export default function Custom() {
+  const onCameraConfirm = (d) => {
+    const url = `http://10.11.200.109:5000/faceapi/detect/liveliness`;
+    const payload = {
+      faceImage: d,
+    };
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(ex => console.log(ex.toString()))
+  };
+
   return (
-    <div className='m-4'>
-        <Camera onConfirm={(d) => console.log(d)}/>
+    <div className="m-4">
+      <Camera onConfirm={onCameraConfirm} />
     </div>
-  )
+  );
 }
