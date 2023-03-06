b7f19cc2c125895c1503cc432eb7564b4bfea379
ovichowdhury <ovi.chowdhury@hotmail.com>
2023-03-06 14:57:48 +0600

    feat: loading component design added

---
+++ components/common/Staging.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Staging() {
+  return (
+    <div className=" h-screen w-full flex justify-center items-center text-center">
+      <span className="bg-[url('/loading.gif')] bg-no-repeat bg-cover h-20 w-20"></span>
+    </div>
+  );
+}
---
+++ pages/staging/index.js
@@ -0,0 +1,6 @@
+import Staging from "@components/common/Staging";
+import React from "react";
+
+export default function Index() {
+  return <Staging />;
+}
